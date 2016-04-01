945ca54e2b10c592758a905c9f911b3e2c231929
Joël Quenneville <joelq@thoughtbot.com>
2016-04-01 10:59:52 -0400

    Add a social status
    All Romans are part of family. That family has one of two social
    statuses: Patrician or Plebian. Plebians were a larger proportion of the
    population we weight the random generation 70/30 towards Plebians
    instead of using a "fair" 50/50 roll.
    
    A nomen is now associated with a family. For higher historical accuracy,
    we generate random families based on their social status from lists of
    historical plebian/patrician families.

--- Random/Roman.elm
+++ Random/Roman.elm
@@ -3,22 +3,21 @@ module Random.Roman where
 import Random exposing (Generator, andThen)
 import Random.Extra as RandomE
 import Random.Maybe as RandomM
-import Roman exposing (Roman)
+import Random.Odds exposing(rollOdds)
+
+import Roman exposing (Roman, Family, SocialStatus(..))
 
 roman : Generator Roman
 roman =
   let nickNames' = cognomen `andThen` nickNames
-      roman' pn n (cn, an) = Roman pn n cn an
+      family' = socialStatus `andThen` family
+      roman' pn f (cn, an) = Roman pn f cn an
   in
-     Random.map3 roman' praenomen nomen nickNames'
+     Random.map3 roman' praenomen family' nickNames'
 
 praenomen : Generator String
 praenomen =
   RandomE.selectWithDefault "Marcus" ["Marcus", "Quintus", "Gaius"]
-
-nomen : Generator String
-nomen =
-  RandomE.selectWithDefault "Julius" ["Julius", "Fabius", "Junius"]
 
 cognomen : Generator (Maybe String)
 cognomen =
@@ -35,3 +34,45 @@ nickNames cognomen =
   case cognomen of
     Just _ -> Random.map (\agnomen' -> (cognomen, agnomen')) agnomen
     Nothing -> RandomE.constant (Nothing, Nothing)
+
+socialStatus : Generator SocialStatus
+socialStatus =
+  rollOdds 70 Plebian Patrician
+
+family : SocialStatus -> Generator Family
+family status =
+  case status of
+    Patrician -> patrician
+    Plebian -> plebian
+
+patrician : Generator Family
+patrician =
+  RandomE.selectWithDefault defaultPatricianFamily patricianFamilies
+
+plebian : Generator Family
+plebian =
+  RandomE.selectWithDefault defaultPlebianFamily plebianFamilies
+
+patricianFamilies : List Family
+patricianFamilies =
+  [ Family Patrician "Julius"
+  , Family Patrician "Fabius"
+  , Family Patrician "Junius"
+  , Family Patrician "Aemelius"
+  ]
+
+defaultPatricianFamily : Family
+defaultPatricianFamily =
+  Family Patrician "Julius"
+
+plebianFamilies : List Family
+plebianFamilies =
+  [ Family Plebian "Octavius"
+  , Family Plebian "Marius"
+  , Family Plebian "Livius"
+  , Family Plebian "Domitius"
+  ]
+
+defaultPlebianFamily : Family
+defaultPlebianFamily =
+  Family Plebian "Octavius"
