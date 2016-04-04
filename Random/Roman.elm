a15ec2352239e2cc00f0d386308dda3794f4a069
Joël Quenneville <joelq@thoughtbot.com>
2016-04-04 07:51:10 -0400

    Extract family-related random to own module
    The `Random.Roman` module was getting rather long and a lot of the
    functions dealt with generating a random family. This extracts those
    functions into a separate `Random.Family` module.

--- Random/Roman.elm
+++ Random/Roman.elm
@@ -4,6 +4,7 @@ import Random exposing (Generator, andThen)
 import Random.Extra as RandomE
 import Random.Maybe as RandomM
 import Random.Odds exposing(rollOdds)
+import Random.Family exposing (socialStatus, family)
 
 import Roman exposing (Roman, Family, SocialStatus(..))
 
@@ -66,45 +67,3 @@ names family =
       praenomen' = familyPraenomen family
   in
      Random.map2 (\pn (cn, an) -> (pn, family, cn, an)) praenomen' nickNames'
-
-socialStatus : Generator SocialStatus
-socialStatus =
-  rollOdds 70 Plebian Patrician
-
-family : SocialStatus -> Generator Family
-family status =
-  case status of
-    Patrician -> patrician
-    Plebian -> plebian
-
-patrician : Generator Family
-patrician =
-  RandomE.selectWithDefault defaultPatricianFamily patricianFamilies
-
-plebian : Generator Family
-plebian =
-  RandomE.selectWithDefault defaultPlebianFamily plebianFamilies
-
-patricianFamilies : List Family
-patricianFamilies =
-  [ Family Patrician "Julius" ["Caesar", "Iulus"] ["Lucius", "Gaius", "Sextus"]
-  , Family Patrician "Fabius" ["Maximus", "Licinus"] ["Caeso", "Quintus", "Marcus"]
-  , Family Patrician "Junius" ["Brutus", "Silanus"] ["Marcus", "Decimus", "Lucius"]
-  , Family Patrician "Aemelius" ["Paulus", "Lepidus"] ["Lucius", "Marcus", "Quintus"]
-  ]
-
-defaultPatricianFamily : Family
-defaultPatricianFamily =
-  Family Patrician "Julius" ["Caesar", "Iulus"] ["Lucius", "Gaius", "Sextus"]
-
-plebianFamilies : List Family
-plebianFamilies =
-  [ Family Plebian "Octavius" ["Rufus"] ["Gnaeus", "Gaius", "Marcus"]
-  , Family Plebian "Marius" [] ["Gaius", "Lucius", "Sextus"]
-  , Family Plebian "Livius" ["Drusus"] ["Gaius", "Lucius", "Titus"]
-  , Family Plebian "Domitius" ["Calvinus", "Ahenobarbus"] ["Gnaeus", "Marcus", "Lucius"]
-  ]
-
-defaultPlebianFamily : Family
-defaultPlebianFamily =
-  Family Plebian "Octavius" ["Rufus"] []
