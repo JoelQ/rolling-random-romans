9fa1cba76f5362d6a43621ea16cff637db5058c3
Joël Quenneville <joelq@thoughtbot.com>
2016-04-07 15:14:54 -0400

    Add basic styling to page
    Add some colors, borders, and layout.

--- Roman.elm
+++ Roman.elm
@@ -1,6 +1,6 @@
 module Roman where
-import Html exposing (Html, span, p, text, div, img)
-import Html.Attributes exposing(property, src)
+import Html exposing (Html, span, p, text, div, img, small, h2)
+import Html.Attributes exposing(property, src, class)
 import Json.Encode
 import String
 
@@ -37,14 +37,17 @@ genderedNomen gender nomen =
 view : Roman -> Html
 view roman =
   div []
-  [ p []
+  [ div [class "name"]
     [ span [property "innerHTML" (Json.Encode.string <| genderSymbol roman)] []
     , span [] [ text (name roman) ]
     ]
-  , div []
-    [ img [ src roman.family.image.url ] []
-    , span [] [ text roman.family.image.attributionText ]
-    , span [] [ text roman.family.image.description ]
+  , div [class "family-detail"]
+    [ h2 [] [ text ("Famous member of gens " ++ roman.family.nomen ) ]
+    , div [ class "image-with-attribution" ]
+      [ img [ src roman.family.image.url ] []
+      , p [] [ small[] [ text roman.family.image.attributionText ]]
+      ]
+    , p [ class "description" ] [ text roman.family.image.description ]
     ]
   ]
 
